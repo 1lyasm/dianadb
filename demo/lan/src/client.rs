c6ff9dfb55d3d23449f62777880a97ef24be8a1c
1lyasm <il.mystafa@gmail.com>
2023-10-14 18:59:50 +0300

    update lan client

--- demo/lan/src/client.rs
+++ demo/lan/src/client.rs
@@ -2,7 +2,7 @@ use std::io::Write;
 
 fn main() {
     println!("Hello, world!");
-    let mut stream = std::net::TcpStream::connect("172.31.22.11:6789".to_owned()).unwrap();
+    let mut stream = std::net::TcpStream::connect("172.31.28.159:6789".to_owned()).unwrap();
     stream.write_all("hi".as_bytes()).unwrap();
 }
 
