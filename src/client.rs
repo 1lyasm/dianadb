867ab0161541163ed1a3f0c0bd17c9eb757f054f
1lyasm <il.mystafa@gmail.com>
2023-10-15 14:55:05 +0300

    make client a module

--- src/client.rs
+++ src/client.rs
@@ -1,18 +1,14 @@
 use std::io::Write;
-
 #[macro_use]
 extern crate log;
-
 #[macro_use]
 mod util;
-
 #[derive(serde::Serialize, serde::Deserialize)]
 struct Config {
     shard_count: usize,
     addresses: Vec<String>,
     pools: Vec<usize>,
 }
-
 impl Config {
     fn init_shard_count(&mut self) {
         self.shard_count = std::env::args()
@@ -21,7 +17,6 @@ impl Config {
             .parse()
             .expect(&format!("{}: invalid shard_count", function!()));
     }
-
     fn validate(&self) {
         let mut is_valid = true;
         let server_count = self.addresses.len();
@@ -36,14 +31,12 @@ impl Config {
             panic!("{}: invalid conf", function!());
         }
     }
-
     fn init_addresses(&mut self) {
         std::io::stdin()
             .lines()
             .for_each(|line| self.addresses.push(line.unwrap()));
         self.validate();
     }
-
     fn init_pools(&mut self) {
         let (mut current_pool, mut j) = (0, 0);
         let server_count = self.addresses.len();
@@ -57,7 +50,6 @@ impl Config {
             j += 1;
         }
     }
-
     fn merge_by_pools(&self) -> Vec<String> {
         let mut pool_addresses = Vec::new();
         for _ in 0..self.shard_count {
@@ -71,15 +63,14 @@ impl Config {
         }
         return pool_addresses;
     }
-
     fn send(&self, address: &String, pool: &usize, peers: &String) {
         info!("{}: sending config to {}", function!(), address);
         let mut stream = std::net::TcpStream::connect(address)
             .expect(&format!("{}: connect failed", function!()));
-        stream.write_all(format!("{} {}", pool.to_string(), peers).as_bytes())
+        stream
+            .write_all(format!("{} {}", pool.to_string(), peers).as_bytes())
             .expect(&format!("{}: write_all failed", function!()));
     }
-
     fn send_all(&self) {
         let pool_addresses = self.merge_by_pools();
         for i in 0..self.addresses.len() {
@@ -89,7 +80,6 @@ impl Config {
             self.send(address, pool, &peers);
         }
     }
-
     fn init(&mut self) {
         self.init_shard_count();
         self.init_addresses();
@@ -101,15 +91,24 @@ impl Config {
         );
     }
 }
-
-fn main() {
-    env_logger::init();
-    info!("{}: client started", function!());
-    let mut conf = Config {
-        shard_count: 0,
-        addresses: Vec::new(),
-        pools: Vec::new(),
-    };
-    conf.init();
-    conf.send_all();
+mod dianadb {
+    struct Client {
+        conf: Config,
+    }
+    impl Client {
+        fn run_statement(&self, statement: &String) {}
+        fn connect() -> Client {
+            env_logger::init();
+            info!("{}: client started", function!());
+            let mut conf = Config {
+                shard_count: 0,
+                addresses: Vec::new(),
+                pools: Vec::new(),
+            };
+            conf.init();
+            conf.send_all();
+            return Client{conf};
+        }
+    }
 }
+
